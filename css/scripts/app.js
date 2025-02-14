767541b1f27b9609ccdd356911c4738e3a3df135
KevinJustinXavier <kevinjxavier@gmail.com>
2025-02-13 19:54:16 -0500

    Lab Assessment 1 KX

---
+++ css/scripts/app.js
@@ -0,0 +1,7 @@
+let studentFullName = 'Kevin Justin Xavier';
+let studentNumber = 1196273;
+const result = studentFullName + studentNumber;
+console.log(result);
+let headrerContent = document.querySelector("h1");
+console.log(headrerContent.classlist)
+
